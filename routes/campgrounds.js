21d8d5b275b9e4001ab35340fb5f0b9b6db1d0ea
Killua <devtewari03@gmail.com>
2023-02-16 14:29:57 +0530

    First Commit

---
+++ routes/campgrounds.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
+const Campground = require('../models/campground');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+
+
+
+router.get('/', catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', { campgrounds })
+}));
+
+router.get('/new', isLoggedIn, (req, res) => {
+    res.render('campgrounds/new');
+});
+
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
+    await campground.save();
+    req.flash('success', 'Successfully made a new campground');
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+router.get('/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    //console.log(id);
+    const campground = await Campground.findById(id).populate(
+        {
+            path: 'reviews',
+            populate: {
+                path: 'author'
+            }
+        }).populate('author');
+    console.log(campground);
+    if (!campground) {
+        //console.log(id);
+        req.flash('error', 'Campground not found');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground })
+}));
+
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground) {
+        //console.log(id);
+        req.flash('error', 'Cannot find that campground');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/edit', { campground });
+}));
+
+router.put('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash('success', 'Successfully updated the campground');
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground) {
+        //console.log(id);
+        req.flash('error', 'Cannot find that campground');
+        return res.redirect('/campgrounds');
+    }
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    const deletedcampground = await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground');
+    res.redirect('/campgrounds');
+}));
+
+module.exports = router;
---
+++ seeds/index.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const num = Math.floor(Math.random() * 100);
+        const camp = new Campground({
+            author: ['63eb1314fe370a068a29a956', '63eb27b77770d5f660477940', '63eb27d9cafb53c89feea7aa'][Math.floor(Math.random() * 3)],
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://source.unsplash.com/featured/?forest&${num}`,
+            description: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id dicta distinctio debitis fuga impedit soluta quo voluptatum error tempore aut aliquam enim dolore, ipsa voluptatem praesentium placeat quis quasi fugiat.',
+            price: price //or just price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
